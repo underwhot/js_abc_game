bd67ccb78b962ad1ddfd85f4e47bca5c9c3cf2fb
Artem Petrovskiy <artpetrovskyi@gmail.com>
2023-07-23 17:45:40 +0300

    complite functional

--- js.js
+++ js.js
@@ -8,3 +8,37 @@
 3-Помните про свойства, получения соседних элементов (previousElementSibling, previousElementSibling и т.д)
 */
 
+document.addEventListener('DOMContentLoaded', function(e) {
+  const boxes = document.querySelectorAll('.boxes__box');
+  const audioClick = new Audio('./audio/Mountain Audio - Menu Click.mp3');
+  const audioWin = new Audio('./audio/huge win.wav');
+  
+  boxes.forEach(function(box) {
+    box.addEventListener('click', function(e) {
+      const target = e.target;
+      const prevEl = target.previousElementSibling;
+  
+      if (prevEl) {
+        prevEl.before(target);
+      } else {
+        const nextEl = target.nextElementSibling;
+        nextEl.after(target);
+      }
+  
+      audioClick.play();
+  
+      checkOrder(document.querySelectorAll('.boxes__box'));
+    });
+  });
+  
+  function checkOrder(item) {
+    if (item[0].classList.contains('box_a') 
+    && item[1].classList.contains('box_b')
+    && item[2].classList.contains('box_c')
+    && item[3].classList.contains('box_d')
+    && item[4].classList.contains('box_e')
+    && item[5].classList.contains('box_f')) {
+      audioWin.play();
+    } 
+  };
+});
